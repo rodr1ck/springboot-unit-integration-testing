5e7ce24fa17c2c75fc406faadd717b6d19398344
rodrigodor <rodrigo.orelana@gmail.com>
2022-02-10 15:47:34 -0300

    JUnit test to get employee by email

--- src/main/java/com/rodrigoguides/springboot/repository/EmployeeRepository.java
+++ src/main/java/com/rodrigoguides/springboot/repository/EmployeeRepository.java
@@ -3,5 +3,9 @@ package com.rodrigoguides.springboot.repository;
 import com.rodrigoguides.springboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
 }
